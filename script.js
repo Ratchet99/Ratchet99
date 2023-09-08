7707b0e5198d2185edccc3909f1956efb62e76e6
SHAMANTH <84974532+SHA-SHAMANTH@users.noreply.github.com>
2023-09-08 21:20:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript can be used to change text after a certain time
+// For example, after 20 seconds, you can replace the text
+
+setTimeout(() => {
+    document.querySelector('h1').textContent = "Welcome to My Portfolio";
+}, 20000);
