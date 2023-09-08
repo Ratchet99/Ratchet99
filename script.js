8a36b47e3642d770446efdb59a560eec2fe80e58
SHAMANTH <84974532+SHA-SHAMANTH@users.noreply.github.com>
2023-09-08 21:40:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // JavaScript can be used to change text after a certain time
 // For example, after 20 seconds, you can replace the text
 
-setTimeout(() => {
-    document.querySelector('h1').textContent = "Welcome to My Portfolio";
-}, 20000);
+document.getElementById('name').addEventListener('click', function() {
+    this.classList.toggle('netflixintrof');
+});
